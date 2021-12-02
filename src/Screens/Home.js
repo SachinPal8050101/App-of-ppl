3a70d880517fceb76e7111b5622e89e28570d197
sachinpal8050101 <sachin.kumar1@daffodilsw.com>
2021-12-02 15:33:54 +0530

    limitpost

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -10,32 +10,69 @@ import {
   TextInput,
   Pressable,
   ScrollView,
+  FlatList
 } from "react-native";
 import UploadPost from "../Component/UploadPost";
 import UploadPostContent from "../Component/UploadPostContent";
 import Post from "../Component/Post";
 import axios from "axios";
+
+import config from "../../src/config";
+
 const Home = () => {
   
   const [upload, setUpload] = useState(false);
   const [posts, setPosts] = useState([]);
-  axios
-    .post("http://192.168.100.135:5000/getPost", {})
-    .then((res) => {
-      setPosts(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      alert(err);
-    });
+   const [skip,setSkip]=useState(0)
+  // var skip=0
+  // axios
+  //   .post(`${config.SERVER_URL}/getPost`, {})
+  //   .then((res) => {
+  //     setPosts(res.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     alert(err);
+  //   });
+  const getPostDataFunction=(skip)=>{
+  axios({
+    url: `${config.SERVER_URL}/getPost`,
+    method: 'post',
+     data:  { skip: skip },
+  })
+  .then((res) => {
+    //  console.log('kkkkkkk-',res)
+     setPosts(res.data)
+  })
+ }
+ useEffect(()=>{
+  getPostDataFunction(skip)
+ })
+
   return (
     <View style={styles.container}>
       <View style={styles.post}>
-        <ScrollView>
-          {posts.map((item) => {
+        {/* <ScrollView> */}
+          {/* {posts.map((item) => {
             return <Post setUpload={setUpload} upload={Post} item={item} />;
-          })}
-        </ScrollView>
+          })} */}
+          <FlatList
+        data={posts}
+        renderItem={({item,index,separators})=><Post  item={item}/>}
+        keyExtractor={item => item._id}
+        onEndReached={({distanceFromEnd})=>{
+          console.log('-----------',distanceFromEnd) 
+          if(distanceFromEnd<100)
+          {
+            console.log('ggggggg')
+            getPostDataFunction(skip)
+          }
+          setSkip(skip+2)
+          // skip=skip+5
+        }}
+        onEndReachedThreshold={0.5}
+      />
+        {/* </ScrollView> */}
       </View>
     </View>
   );
